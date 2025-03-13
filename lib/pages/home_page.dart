4cdefff559e7174605cd238e0c62236f4db80071
Toretay <abdymvzhit@gmail.com>
2025-03-13 12:25:29 +0500

    fetched comments from internet and displayed it

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider_test/models/comment.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<List<Comment>> fetchData() async {
+    var response = await http.get(
+      Uri.https("jsonplaceholder.typicode.com", "/comments"),
+    );
+    if (response.statusCode == 200) {
+      return commentFromJson(response.body);
+    } else {
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: FutureBuilder(
+        future: fetchData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var comments = snapshot.data;
+            return ListView.builder(
+              itemCount: comments!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(comments[index].body),
+                  subtitle: Text(comments[index].email),
+                );
+              },
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      ),
+    );
   }
 }
