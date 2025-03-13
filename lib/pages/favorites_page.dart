78feb0ef5ce2743bea9696324ce6de333ba7d5ff
Toretay <abdymvzhit@gmail.com>
2025-03-13 15:48:54 +0500

    created consumer to output the favs.

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_test/models/favorite_model.dart';
 
 class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
@@ -12,6 +14,33 @@ class _FavoritesPageState extends State<FavoritesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Favorites"), centerTitle: true),
+      body: Consumer<FavoriteModel>(
+        builder: (context, value, child) {
+          var favorites = value.favorites.reversed.toList();
+          return ListView.builder(
+            itemCount: favorites.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  favorites[index].body,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                subtitle: Text(favorites[index].email),
+                trailing: IconButton(
+                  onPressed: () {
+                    value.addRemove(favorites[index]);
+                  },
+                  icon: Icon(
+                    favorites.contains(favorites[index])
+                        ? Icons.favorite
+                        : Icons.favorite_border,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
