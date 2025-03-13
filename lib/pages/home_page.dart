b13eed0971421b7313b37ee327266ebb1104f8eb
Toretay <abdymvzhit@gmail.com>
2025-03-13 12:29:01 +0500

    added appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -32,6 +32,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Comments"),
+      ),
       body: FutureBuilder(
         future: dataFuture,
         builder: (context, snapshot) {
